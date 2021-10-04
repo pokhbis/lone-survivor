af468fffeeaa409e9d928a8f7f4c72d8903c4988
pokhbis <prokash748@gmail.com>
2021-10-04 17:57:32 +0600

    final commit

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import logo from '../../Images/survival-logo-1-removebg-preview.png';
 import './Header.css';
 const Header = () => {
@@ -17,10 +18,14 @@ const Header = () => {
                     </div>
 
                     <Nav className="me-end">
-                        <Nav.Link className="header-options f" href="/home">Home</Nav.Link>
+                        {/* <Nav.Link className="header-options f" href="/home">Home</Nav.Link>
                         <Nav.Link className="header-options" href="/about">About</Nav.Link>
                         <Nav.Link className="header-options" href="/service">Services</Nav.Link>
-                        <Nav.Link className="header-options" href="/blog">Blog</Nav.Link>
+                        <Nav.Link className="header-options" href="/blog">Blog</Nav.Link> */}
+                        <Link className="header-options" to="/home">Home</Link>
+                        <Link className="header-options" to="/about">About</Link>
+                        <Link className="header-options" to="/service">Services</Link>
+                        <Link className="header-options" to="/blog">Blog</Link>
                     </Nav>
                 </Container>
             </Navbar>
